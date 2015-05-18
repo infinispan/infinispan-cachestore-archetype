de5558bc4321d12987f3e49e806657221b1e838a
Tristan Tarrant <ttarrant@redhat.com>
2015-05-18 10:40:56 +0200

    Update cache store archetypes
    - Include detailed documentation within the methods of the custom
    loaders/stores
    - Provide a concrete example of parsing elements/attributes
    - Add the JBoss repositories

--- src/main/resources/archetype-resources/cache-loader/src/main/java/CustomStoreConfigurationBuilder.java
+++ src/main/resources/archetype-resources/cache-loader/src/main/java/CustomStoreConfigurationBuilder.java
@@ -5,25 +5,39 @@ package ${package};
 import org.infinispan.configuration.cache.AbstractStoreConfigurationBuilder;
 import org.infinispan.configuration.cache.PersistenceConfigurationBuilder;
 
+/**
+ * CustomStoreConfigurationBuilder.
+ * 
+ * This class will be used by the ConfigurationBuilder to implement fluent configuration for the custom CacheLoader / CacheWriter.
+ * Methods should use the fluent style, rather than the setter/getter style and should return an instance of this object.
+ * 
+ */
+public class CustomStoreConfigurationBuilder extends AbstractStoreConfigurationBuilder<CustomStoreConfiguration, CustomStoreConfigurationBuilder> {
 
-public class CustomStoreConfigurationBuilder extends AbstractStoreConfigurationBuilder<CustomStoreConfiguration, CustomStoreConfigurationBuilder>{
+    String sampleAttribute;
 
     public CustomStoreConfigurationBuilder(
             PersistenceConfigurationBuilder builder) {
         super(builder);
-        // TODO Auto-generated constructor stub
+    }
+    
+    public CustomStoreConfigurationBuilder sampleAttribute(String sampleAttribute) {
+        this.sampleAttribute = sampleAttribute;
+        return this;
     }
 
     @Override
     public CustomStoreConfiguration create() {
-        // TODO Auto-generated method stub
-        return null;
+        /*
+         * This method should construct a new instance of a CustomStoreConfiguration object. There will be one instance per cache.
+         */
+        return new CustomStoreConfiguration(sampleAttribute, purgeOnStartup, fetchPersistentState, ignoreModifications, async.create(),
+              singletonStore.create(), preload, shared, properties);
     }
 
     @Override
     public CustomStoreConfigurationBuilder self() {
-        // TODO Auto-generated method stub
-        return null;
+        return this;
     }
 
 }
--- src/main/resources/archetype-resources/cache-writer/src/main/java/CustomCacheWriter.java
+++ src/main/resources/archetype-resources/cache-writer/src/main/java/CustomCacheWriter.java
@@ -13,34 +13,60 @@ import org.kohsuke.MetaInfServices;
 @ConfiguredBy(CustomStoreConfiguration.class)
 public class CustomCacheWriter<K,V> implements CacheWriter<K, V> {
 
-    @Override
-    public void start() {
-        // TODO Auto-generated method stub
-        
-    }
+   @Override
+   public void init(InitializationContext ctx) {
+       /*
+        * This method will be invoked by the PersistenceManager during initialization. The InitializationContext
+        * contains:
+        * - this CacheWriter's configuration
+        * - the cache to which this loader is applied. Your loader might want to use the cache's name to construct 
+        *   cache-specific identifiers
+        * - the StreamingMarshaller that needs to be used to marshall/unmarshall the entries
+        * - a TimeService which the loader can use to determine expired entries
+        * - a ByteBufferFactory which needs to be used to construct ByteBuffers
+        * - a MarshalledEntryFactory which needs to be used to construct entries from the data retrieved by the loader 
+        */
+   }
+
+   @Override
+   public void start() {
+       /*
+        * This method will be invoked by the PersistenceManager to start the CacheWriter. At this stage configuration
+        * is complete and the loader can perform operations such as opening a connection to the external storage,
+        * initialize internal data structures, etc.
+        */
+   }
+
+   @Override
+   public void stop() {
+       /*
+        * This method will be invoked by the PersistenceManager to stop the CacheWriter. The CacheWriter should close any
+        * connections to the external storage and perform any needed cleanup.
+        */
+   }
 
     @Override
-    public void stop() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public boolean delete(Object arg0) {
-        // TODO Auto-generated method stub
+    public boolean delete(Object key) {
+        /*
+         * The CacheWriter should remove from the external storage the entry identified by the specified key.
+         * 
+         * Note that keys will be in the cache's native format, which means that if the cache is being used by a remoting protocol
+         * such as HotRod or REST and compatibility mode has not been enabled, then they will be encoded in a byte[]. 
+         */
         return false;
     }
 
     @Override
-    public void init(InitializationContext arg0) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void write(MarshalledEntry<? extends K, ? extends V> arg0) {
-        // TODO Auto-generated method stub
-        
+    public void write(MarshalledEntry<? extends K, ? extends V> marshalledEntry) {
+        /*
+         * The CacheWriter should write the specified entry to the external storage. 
+         * 
+         * The PersistenceManager uses MarshalledEntry as the default format so that CacheWriters can efficiently store data coming 
+         * from a remote node, thus avoiding any additional transformation steps.
+         * 
+         * Note that keys and values will be in the cache's native format, which means that if the cache is being used by a remoting protocol
+         * such as HotRod or REST and compatibility mode has not been enabled, then they will be encoded in a byte[].
+         */
     }
 
 }
